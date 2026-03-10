b27ee478d4a2697f659eb50383559ac045f178cb
Thalia <20241ctb0180018@estudantes.ifpr.edu.br>
2026-03-10 17:15:04 -0300

    versão1

---
+++ FazendeiroUnity/Assets/Scripts/NewPlayerControl.cs
@@ -0,0 +1,86 @@
+using System.Collections;
+using System.Collections.Generic;
+using Microsoft.Unity.VisualStudio.Editor;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class NewPlayerControl : MonoBehaviour
+{
+    public float speed = 20f;
+    public float xRange = 15f;
+    public GameObject projectilePrefab;
+    //float horizontalInput;
+    public InputActionAsset InputActions;
+    private InputAction moveAction;
+    private InputAction fireAction;
+    private InputAction pauseAction;
+    public GameObject menuPause;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalInput = moveAction.ReadValue<Vector2>().x;
+        //float horizontalInput = Input.GetAxis("Horizontal");
+        // movimenta o player para esquerda e direita a partir da entrada do usu�rio
+        transform.Translate(Vector3.right * speed * Time.deltaTime * horizontalInput);
+        // mant�m o player dentro dos limites do jogo (eixo x)
+        if (transform.position.x < -xRange)
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.y);
+        }
+        if (transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.y);
+        }
+        if (fireAction.WasPressedThisFrame())
+        {
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+        }
+         if (pauseAction.WasPressedThisFrame())
+        {
+            Pause();
+        }
+        // dispara comida ao pressionar barra de espa�o
+        /*if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+        }*/
+        
+    }
+
+    /*public void MoveEvent(InputAction.CallbackContext context)
+    {
+        horizontalInput = context.ReadValue<Vector2>().x;
+    }
+
+    public void FireEvent(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+        }
+        
+    }*/
+
+    private void Onable()
+    {
+        InputActions.FindActionMap("Player").Enable();
+    }
+    private void OnDisable()
+    {
+        InputActions.FindActionMap("Player").Disable();
+    }
+    private void Awake()
+    {
+        moveAction = InputSystem.actions.FindAction("Move");
+        fireAction = InputSystem.actions.FindAction("Jump");
+        pauseAction = InputSystem.actions.FindAction("Pause");
+    }
+    private void Pause()
+    {
+        OnDisable();
+        menuPause.gameObject.SetActive(true);
+
+    }
+}
