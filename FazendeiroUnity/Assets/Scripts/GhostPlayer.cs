4ac61aa8bd4d2b937d1bdd078d8fd640e8fb07ae
Thalia <20241ctb0180018@estudantes.ifpr.edu.br>
2026-04-07 17:16:19 -0300

    Versão8
    Ghost e corrotina

--- FazendeiroUnity/Assets/Scripts/GhostPlayer.cs
+++ FazendeiroUnity/Assets/Scripts/GhostPlayer.cs
@@ -6,15 +6,16 @@ using System.Collections;
 
 public class GhostPlayer : MonoBehaviour
 {
-    private InputAction ghostAction;
     private bool ghosttt = false;
+    private bool endTime = true;
     private Renderer playerRender;
-
+    private InputAction ghostAction;
+    public InputActionAsset InputActions;
    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        playerRender = GetComponent<Renderer>();
     }
 
     // Update is called once per frame
@@ -32,27 +33,46 @@ public class GhostPlayer : MonoBehaviour
         ghostAction = InputSystem.actions.FindAction("Ghost");
     }
 
+    private void OnEnable()
+    {
+        InputActions.FindActionMap("Player").Enable();
+    }
+    private void OnDisable()
+    {
+        InputActions.FindActionMap("Player").Disable();
+    }
+
     private void Ghost()
     {
         if (!ghosttt)
         {
             playerRender.enabled=false;
             ghosttt=true;
+            StartCoroutine(TerminaGhost());
         } else
         {
             playerRender.enabled=true;
             ghosttt=false;
+            StartCoroutine(ComecaGhost());
         }
 
         //Debug.Log("Ativou Ghost");
-
-       // StartCoroutine("TerminaGhost");
     }
 
-    /*IEnumerator TerminaGhost()
+    IEnumerator TerminaGhost()
     {
-        yield return new WaitForSeconds(3f);
-        Debug.Log("Desativou Ghost");
-    }*/
+        yield return new WaitForSeconds(2f);
+        if (ghosttt)
+        {
+            Ghost();
+        }
+    }
+    IEnumerator ComecaGhost()
+    {
+        endTime = false;
+        yield return new WaitForSeconds(2f);
+        endTime = true;
+
+    }
 
 }
