0a99d97dfa1ff4b181a6759f21231e52f2510474
Thalia <20241ctb0180018@estudantes.ifpr.edu.br>
2026-04-08 11:53:45 -0300

    Versão14
    Pontuação completa e começo da pontuação da vida

--- FazendeiroUnity/Assets/Scripts/Scoree.cs
+++ FazendeiroUnity/Assets/Scripts/Scoree.cs
@@ -4,8 +4,16 @@ using UnityEngine.UI;
 
 public class Scoree : MonoBehaviour
 {
+    public static Scoree instance;
+    
     public TextMeshProUGUI scoreText;
+
     int score=0;
+
+    private void Awake()
+    {
+        instance = this;
+    }
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -13,8 +21,10 @@ public class Scoree : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    public void AddPontos()
     {
-        
+        score += 1;
+        scoreText.text = score.ToString() + " PONTOS";
+
     }
 }
---
+++ FazendeiroUnity/Assets/Scripts/VidaManeger.cs
@@ -0,0 +1,20 @@
+using TMPro;
+using UnityEngine;
+
+public class VidaManeger : MonoBehaviour
+{
+    public TextMeshProUGUI vidaText;
+
+    int vida = 0;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
